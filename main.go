d4131db16aac08cdf140a7537da3fafb60448da5
Walid Siala <wsiala4@gmail.com>
2025-01-04 17:54:35 +0100

    Added ListTodos function with tabular format

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/walids35/todo-cli/todo"
+import (
+	"github.com/walids35/todo-cli/todo"
+)
 
-const filename string = "../data.csv"
+const filename string = "data.csv"
 
 func main(){
-	todo.LoadFromCSVFile(filename)
+	var todos []todo.Todo
+	todos, err := todo.LoadFromCSVFile(filename)
+	if err != nil {
+		todos = []todo.Todo{}
+	}
+	todo.ListTodos(todos)
 }
