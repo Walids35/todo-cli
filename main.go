817abd3de2f4ec63032afa3b209bc4d369311613
Walid Siala <wsiala4@gmail.com>
2025-01-04 17:19:38 +0100

    Added todo package

--- main.go
+++ main.go
@@ -1,33 +1,9 @@
 package main
 
-import (
-	"encoding/csv"
-	"fmt"
-	"log"
-	"os"
-)
+import "github.com/walids35/todo-cli/todo"
 
-const filename string = "data.csv"
+const filename string = "../data.csv"
 
 func main(){
-	LoadTasks(filename)
-}
-
-func LoadTasks(filename string) {
-	data, err := os.Open(filename)
-
-	if err != nil {
-		log.Fatal("Error while reading the file", err)
-	}
-
-	reader := csv.NewReader(data)
-	records, err := reader.ReadAll()
-
-	if err != nil{
-		fmt.Println("Error reading records")
-	}
-
-	for _,record := range records{
-		fmt.Println(record)
-	}
+	todo.LoadFromCSVFile(filename)
 }
