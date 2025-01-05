359a6c680a8f3ee4241da764eda89b79018b9ab4
Walid Siala <wsiala4@gmail.com>
2025-01-05 13:01:34 +0100

    Added cli execution

--- main.go
+++ main.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-	"github.com/walids35/todo-cli/todo"
+	"github.com/walids35/todo-cli/cmd"
 )
 
-const filename string = "data.csv"
-
 func main(){
-	var todos []todo.Todo
-	todos, err := todo.LoadFromCSVFile(filename)
-	if err != nil {
-		todos = []todo.Todo{}
-	}
-	todo.ListTodos(todos)
-	todo.AddTodo("Clean my bed", &todos)
-	todo.ListTodos(todos)
-	todo.MarkCompleted("1", &todos)
-	todo.ListTodos(todos)
-	todo.DeleteTodo("1", &todos)
-	todo.ListTodos(todos)
+	cmd.Execute()
 }
